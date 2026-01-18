4b0539bff8d9f81aa770b16f7ff7789fb9648646
Alan <alanmathew190@gmail.com>
2026-01-18 11:06:58 +0530

    88 created and completed

---
+++ leetcode/attempting/88.js
@@ -0,0 +1,8 @@
+var merge = function (nums1, m, nums2, n) {
+  for (i = 0; i < n; i++) {
+    nums1[m + i] = nums2[i];
+  }
+  nums1.sort((a, b) => a - b);
+};
+
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
