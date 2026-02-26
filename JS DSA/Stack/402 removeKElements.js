d237f7963698ca97c4eddd9924e90d0639f46ed6
Alan <alanmathew190@gmail.com>
2026-02-26 11:06:35 +0530

    testing

--- JS DSA/Stack/402 removeKElements.js
+++ JS DSA/Stack/402 removeKElements.js
@@ -1,18 +1,23 @@
+// var removeKdigits = function (num, k) {
+//   let stack = [];
+
+//   for (let n of num) {
+//     while (stack.length && stack[stack.length - 1] > n && k > 0) {
+//       stack.pop();
+//       k--;
+//     }
+//     stack.push(n);
+//   }
+//   while (k > 0) {
+//     stack.pop();
+//     k--;
+//   }
+//   return stack === "" ? "0" : stack.join("");
+// };
+// console.log(removeKdigits("1432219", 3));
+
 var removeKdigits = function (num, k) {
-    let stack = []
-    
-    for (let n of num) {
-        while (stack.length && stack[stack.length - 1] > n && k>0) {
-            stack.pop()
-            k--
-        } 
-            stack.push(n)
-      
-    }
-      while (k > 0) {
-        stack.pop();
-        k--;
-      }
-    return stack ===""?"0":stack.join('')
+
+  
 };
 console.log(removeKdigits("1432219", 3));
