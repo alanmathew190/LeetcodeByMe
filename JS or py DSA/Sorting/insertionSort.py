cdd8d716ae7c2531bdbe7c2e51c875d49b26b3f1
Alan <alanmathew190@gmail.com>
2026-03-29 17:31:38 +0530

    just changed the folder name OMFG and started sorting algorithms

---
+++ JS or py DSA/Sorting/insertionSort.py
@@ -0,0 +1,11 @@
+
+
+def insertionSort(arr):
+    for i in range(len(arr)):
+        j=i
+        
+        while j>0 and arr[j-1]>arr[j]:
+            arr[j-1],arr[j]=arr[j],arr[j-1]
+            j-=1        
+    return arr
+print(insertionSort([5,2,4,6,1,3]))
