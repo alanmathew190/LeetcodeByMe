896a573504147beb8e55d8c3d0edbb0bb1a2908e
Alan <alanmathew190@gmail.com>
2026-03-29 17:47:52 +0530

    insertion sort

--- JS or py DSA/Sorting/insertionSort.py
+++ JS or py DSA/Sorting/insertionSort.py
@@ -4,8 +4,8 @@ def insertionSort(arr):
     for i in range(len(arr)):
         j=i
         
-        while j>0 and arr[j-1]>arr[j]:
+        while j>0 and arr[j-1]>arr[j]:  #arr[j-1]>arr[j]: for increasing order and arr[j-1]<arr[j]: for decreasing order
             arr[j-1],arr[j]=arr[j],arr[j-1]
             j-=1        
     return arr
-print(insertionSort([5,2,4,6,1,3]))
+print(insertionSort(list(map(int,input().split()))))
